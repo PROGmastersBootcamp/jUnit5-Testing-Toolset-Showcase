c57648ba8178ca669ebe25750b162343bf42fd43
Magyar Attila <magyar.attila317@gmail.com>
2020-12-08 22:37:07 +0100

    init

---
+++ src/test/java/hu/progmasters/testing/demo_08_benchmark/Benchmarks.java
@@ -0,0 +1,20 @@
+package hu.progmasters.testing.demo_08_benchmark;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(value = 1,
+      warmups = 2)
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+public class Benchmarks {
+
+    @Benchmark
+    public void init() {
+
+    }
+
+}
